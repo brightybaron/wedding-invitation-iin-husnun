65a4c722f76cae750717d49f4392aef3e81306e1
brightybaron <ardi.panda54@protonmail.com>
2025-05-05 15:21:00 +0700

    8th push + blabla

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -62,7 +62,41 @@ export default function AudioPlayer() {
           animationIterationCount: "infinite",
         }}
       >
-        {isPlaying ? <IconDisc /> : <IconPause />}
+        {isPlaying ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-12 h-12"
+          >
+            <circle cx="12" cy="12" r="10"></circle>
+            <path d="M6 12c0-1.7.7-3.2 1.8-4.2"></path>
+            <circle cx="12" cy="12" r="2"></circle>
+            <path d="M18 12c0 1.7-.7 3.2-1.8 4.2"></path>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-8 h-8"
+          >
+            <rect x="14" y="4" width="4" height="16" rx="1" />
+            <rect x="6" y="4" width="4" height="16" rx="1" />
+          </svg>
+        )}
       </div>
     </div>
   );
